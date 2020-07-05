96844dcb3bca80970d4c734d67a6fd28e19cb6df
Tom Mechineau <tom.mechineau@gmail.com>
2020-07-05 18:52:22 +0200

    [Patricia_Trie] Add typical example

--- vague-search-index/src/main.rs
+++ vague-search-index/src/main.rs
@@ -4,14 +4,18 @@ use std::num::NonZeroU32;
 mod patricia_trie;
 mod utils;
 fn main() {
-    println!("Hello, world!");
     let mut parent = PatriciaNode::create_empty();
     parent.insert(&String::from("abc"), NonZeroU32::new(1).unwrap());
     let child = parent.search(String::from("abc"));
     assert!(child.is_some());
     parent.delete(&String::from("abc"));
-    let new_pat = PatriciaNode::create_from_file("test");
+    
+    let new_pat = PatriciaNode::create_from_file("words.txt");
     if let Some(node) = new_pat {
-        println!("{:?}", node);
+        println!("Everything is ok!");
+        println!("{:?}", node.search(String::from("ailley")));
+    }
+    else {
+        println!("Ugh! Shit happened");
     }
 }
